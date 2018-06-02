d160c0881fcc4d3f05e330c91cd6f7841eee416a
Adam Forgacs <adamforgacs256@gmail.com>
2018-06-02 22:32:32 +0200

    Added package-info for Javadoc.

---
+++ bean-mirror-core/src/main/java/com/github/elopteryx/reflect/internal/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The package used by the implementation. Classes of this
+ * package are considered internal.
+ */
+package com.github.elopteryx.reflect.internal;
---
+++ bean-mirror-core/src/main/java/com/github/elopteryx/reflect/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The top level of the package hierarchy which contains the
+ * public API of this library.
+ *
+ * <p>The {@link com.github.elopteryx.reflect.BeanMirror} class
+ * is designed as the entry-point for each operation. There are
+ * two implementations, the {@link com.github.elopteryx.reflect.ObjectMirror}
+ * which handles plain object field and method access, and there
+ * is {@link com.github.elopteryx.reflect.ClassMirror} for creating
+ * new instances and accessing static fields and methods.</p>
+ */
+package com.github.elopteryx.reflect;
