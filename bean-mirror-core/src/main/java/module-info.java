26f2a63205917ecc0938c995ecca946e495d1ebe
Adam Forgacs <adamforgacs256@gmail.com>
2018-09-15 19:32:16 +0200

    Build on release level 11. Remove module descriptor from the tests, not needed anymore.

--- bean-mirror-core/src/main/java/module-info.java
+++ bean-mirror-core/src/main/java/module-info.java
@@ -3,5 +3,4 @@
  */
 module com.github.elopteryx.reflect {
     exports com.github.elopteryx.reflect;
-    exports com.github.elopteryx.reflect.internal to com.github.elopteryx.reflect.tests;
 }
--- bean-mirror-tests/src/test/java/module-info.java
+++
@@ -1,6 +0,0 @@
-
-module com.github.elopteryx.reflect.tests {
-    requires com.github.elopteryx.reflect;
-    requires org.junit.jupiter.api;
-    requires jmh.core;
-}
